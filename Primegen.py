4d40c2e441f4480979d1b336f561e8c3ae7647e9
michaellin <michaelv03@gmail.com>
2012-04-14 19:22:57 -0700

    add primegen.py: Sieve of Atkin implementation

--- Primegen.py
+++ Primegen.py
@@ -1,24 +1,25 @@
-from math import sqrt, mod
+from math import sqrt
+from operator import mod
 
 def SieveAtkin(limit):
-    
-    is_prime = [False]*(limit-2)
-    k = sqrt(limit)
+    is_prime = [False for _ in range(limit + 1)]
+    k = int(sqrt(limit)) + 1
     for i in range(k):
         for j in range(k):
-            n = (4*(i**2) + (k**2))
-            if (n <= limit) and (mod(n, 12) in (1, 5)):
+            n = (4*(i**2) + (j**2))
+            if (n <= limit) and (n % 12 in (1, 5)):
                 is_prime[n] = not is_prime[n]
-            n = (3*(i**2) + (k**2))
-            if (n <= limi) and (mod(n, 12) == 7):
+            n = (3*(i**2) + (j**2))
+            if (n <= limit) and (n % 12 == 7):
                 is_prime[n] = not is_prime[n]
-            n = (3*(i**2) - (k**2))
-            if (i > j) and (n <= limit) and (mod(n, 12) == 11):
-                is_prime[n] = not is_prime[n]
+            if (i > j):
+                n = (3*(i**2) - (j**2))
+                if (n <= limit) and (n % 12 == 11):
+                    is_prime[n] = not is_prime[n]
 
     for num in range(5,k):
         if is_prime[num]:
-            for m in range((num**2), (limit + 1), (num**2)):
+            for m in range((num**2), limit, (num**2)):
                 is_prime[m] = False
 
     primes = [2, 3]
@@ -26,3 +27,5 @@ def SieveAtkin(limit):
         if is_prime[num]:
             primes.append(num)
     return primes
+
+print(len(SieveAtkin(10**8)))
